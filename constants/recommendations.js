d7e3c03ab6a98164704c76abcb1eaf16d6581cff
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-08-30 12:26:02 +0530

    add and modify recomendation styles

--- constants/recommendations.js
+++ constants/recommendations.js
@@ -15,12 +15,24 @@ const recommendations = [
     date: "July 14, 2024",
     name: "Kinjal Dave",
     position: "Scientist",
-    relationship: "worked with Lilan on the same team",
+    relationship: "Kinjal worked with Lilan on the same team",
     image: "assets/img/recommendation/Kinjal Dave.png",
     linkedin: "https://www.linkedin.com/in/kinjal-dave-380954108/",
     quote:
       "I have had the privilege of collaborating with Lilan at the UN COPUOS 2024, as an SGAC Delegate. His exceptional leadership skills and ability to inspire others were evident in every conversation we had.  Lilan is a natural...",
     recommendation:
       "I have had the privilege of collaborating with Lilan at the UN COPUOS 2024, as an SGAC Delegate. His exceptional leadership skills and ability to inspire others were evident in every conversation we had. Lilan is a natural expert and his passion for uses of space for sustainable development of Earth is evident. His dedication towards the vision and mission that he wants to materialise as a business is excellent and inspiring.",
+  },
+  {
+    date: "July 4, 2024",
+    name: "Arpan Yagnik",
+    position: "Associate Professor of Advertising",
+    relationship: "Arpan worked with Lilan on the same team",
+    image: "assets/img/recommendation/Arpan Yagnik.png",
+    linkedin: "https://www.linkedin.com/in/arpan-yagnik-defeatfearbecreative/",
+    quote:
+      "Lilan has a heart of gold. I say so because his compassion extends much beyond him. He is a deeply caring and committed social entrepreneur. He has a proven track record of leadership where he has inspired innovation, delivered...",
+    recommendation:
+      "Lilan has a heart of gold. I say so because his compassion extends much beyond him. He is a deeply caring and committed social entrepreneur. He has a proven track record of leadership where he has inspired innovation, delivered impactful results, and led an intergenerational team. \n\nLilan is a perpetual learner with a growth mindset. He is gentle in his demeanor and firm in his clarity. Through his work he has impacted hundreds of thousands of people on the planet. He has won many prestigious accolades, which are a testament of his committed efforts to provide food security. \n\nLilan is a beacon of hope shining bright in the bleak times. I wish him the best and offer my strongest recommendation for leading the efforts in restoring balance in this immensely imbalanced world.",
   },
 ];
