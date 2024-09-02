caf587540606f4207604d73eb3ec3705890f41df
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-09-02 16:07:18 +0530

    add impact forbes

--- constants/impacts.js
+++ constants/impacts.js
@@ -4,7 +4,7 @@ const impacts = [
     title: "Forbes 30 under 30: Social Impact List",
     image: "assets/img/impact/forbes/ada-derana.png",
     description:
-      "Add overall description of this impact. Add overall description of this impact. Add overall description of this impact. Add overall description of this impact.",
+      "Sri Lankan entrepreneur Lilan Dayananda has been honored with a coveted spot on Forbes Magazine’s “30 Under 30 Asia” list in the social impact sector.",
     slides: [
       {
         date: "May, 29, 2024",
@@ -13,7 +13,7 @@ const impacts = [
         title: "Sri Lankan Entrepreneur Lilan Dayananda in Forbes “30 Under 30 Asia” List",
         image: "assets/img/impact/forbes/ada-derana.png",
         description:
-          "Sri Lankan entrepreneur Lilan Dayananda is among 300 young entrepreneurs, leaders, influencers, and trailblazers across the Asia-Pacific region selected by Forbes for its ninth edition of the “30 Under 30 Asia” list. All individuals on the list are under 30 and are leading the transformation of industries while finding innovative ways to navigate new business realities.",
+          "Sri Lankan entrepreneur Lilan Dayananda is among 300 young entrepreneurs, leaders, influencers, and trailblazers across the Asia-Pacific region selected by Forbes for its ninth edition of the “30 Under 30 Asia” list. All individuals on the list are under 30 and are leading the transformation of industries while finding innovative ways to navigate new business realities. Now in its ninth year, the Asia list is part of the global expansion of the Forbes 30 Under 30 franchise, which started over a decade ago.",
         link: "https://bizenglish.adaderana.lk/sri-lankan-entrepreneur-lilan-dayananda-in-forbes-30-under-30-asia-list/",
         backup_image: "assets/img/impact/forbes/backup/ada-derana.png",
       },
@@ -24,20 +24,139 @@ const impacts = [
         title: "Sri Lankan entrepreneur Lilan Dayananda featured in Forbes 30 Under 30 list",
         image: "assets/img/impact/forbes/our-economics.png",
         description:
-          'Sri Lanka entrepreneur Lilan Dayananda who is the co-founder and CEO of Elzian Agro has been featured in the 9th edition of Forbes\' "30 Under 30 Asia" list under the social impact category.',
+          'Sri Lanka entrepreneur Lilan Dayananda who is the co-founder and CEO of Elzian Agro has been featured in the 9th edition of Forbes\' "30 Under 30 Asia" list under the social impact category. 🇱🇰 👏',
         link: "https://www.linkedin.com/posts/our-economics_oureconomics-forbes-activity-7211230358384640000-5ZJG/?utm_source=share&utm_medium=member_desktop",
         backup_image: "assets/img/impact/forbes/backup/our-economics.png",
       },
       {
-        date: "",
+        date: "May 29, 2024",
         source_name: "Sunday Times",
         source_link: "https://sundaytimes.lk/online/",
         title: "Sri Lanka’s Lilan Dayananda in Forbes “30 Under 30 Asia” List",
-        image: "assets/img/impact/forbes/ada-derana.png",
+        image: "assets/img/impact/forbes/sunday-times.png",
         description:
-          'Sri Lankan entrepreneur Lilan Dayananda has been honored by Forbes as one of the 300 young entrepreneurs, leaders, influencers, and trailblazers across the Asia-Pacific region in its ninth annual "30 Under 30 Asia" list.',
+          'Sri Lankan entrepreneur Lilan Dayananda has been honored by Forbes as one of the 300 young entrepreneurs, leaders, influencers, and trailblazers across the Asia-Pacific region in its ninth annual "30 Under 30 Asia" list. This prestigious list, featuring individuals under the age of 30, highlights those driving industry transformations and pioneering innovative solutions to adapt to evolving business landscapes. Now in its ninth year, the Asia list is an extension of the Forbes 30 Under 30 franchise, which began more than a decade ago.',
         link: "https://sundaytimes.lk/online/business/Sri-Lankas-Lilan-Dayananda-in-Forbes-30-Under-30-Asia-List/10-1145847",
         backup_image: "assets/img/impact/forbes/backup/sunday-times.png",
+      },
+      {
+        date: "May 23, 2024",
+        source_name: "Ceylon Tribune",
+        source_link: "https://ceylontribune.com/",
+        title: "Sri Lankan Entrepreneur Lilan Dayananda Honored in Forbes ‘30 Under 30 Asia’ List",
+        image: "assets/img/impact/forbes/ceylon-tribune.png",
+        description:
+          "Sri Lankan entrepreneur Lilan Dayananda has been honored with a coveted spot on Forbes Magazine’s “30 Under 30 Asia” list in the social impact sector. This prestigious list, now in its ninth year, recognizes young leaders under 30 who are driving innovation and adapting to the evolving business landscape. The Asia list is an extension of the renowned Forbes 30 Under 30 franchise, which has been celebrating young talent for over ten years.",
+        link: "https://ceylontribune.com/?p=895",
+        backup_image: "assets/img/impact/forbes/backup/ceylon-tribune.png",
+      },
+      {
+        date: "May 28, 2024",
+        source_name: "Lanka Puwath",
+        source_link: "https://english.lankapuvath.lk/",
+        title: "Sri Lankan Entrepreneur Lilan Dayananda Honored in Forbes ‘30 Under 30 Asia’ List",
+        image: "assets/img/impact/forbes/lanka-puwath.png",
+        description:
+          "Sri Lankan entrepreneur Lilan Dayananda has been honored with a coveted spot on Forbes Magazine’s “30 Under 30 Asia” list in the social impact sector. This prestigious list, now in its ninth year, recognizes young leaders under 30 who are driving innovation and adapting to the evolving business landscape. The Asia list is an extension of the renowned Forbes 30 Under 30 franchise, which has been celebrating young talent for over ten years.",
+        link: "https://english.lankapuvath.lk/2024/05/28/lilan-dayananda/",
+        backup_image: "assets/img/impact/forbes/backup/lanka-puwath.png",
+      },
+      {
+        date: "May 23, 2024",
+        source_name: "Business Cafe ",
+        source_link: "https://businesscafe.lk/",
+        title: "Forbes '30 Under 30 Asia' list celebrates Sri Lankan Entrepreneur Lilan Dayananda",
+        image: "assets/img/impact/forbes/business-cafe.png",
+        description:
+          "Now in its ninth year, this esteemed list highlights young visionaries under 30 who are revolutionizing their industries and pioneering innovative strategies to meet the demands of a dynamic business environment.",
+        link: "https://businesscafe.lk/business-news/food-beverage/item/15460-forbes-30-under-30-asia-list-celebrates-sri-lankan-entrepreneur-lilan-dayananda",
+        backup_image: "assets/img/impact/forbes/backup/business-cafe.png",
+      },
+      {
+        date: "Jun 02, 2024",
+        source_name: "The Morning",
+        source_link: "https://www.themorning.lk/",
+        title: "Lilan Dayananda: Making a difference through agritech",
+        image: "assets/img/impact/forbes/the-morning.png",
+        description:
+          "Youth having the power to change the world is a commonly-held belief. And every year Forbes magazine recognises youth who are out there making a difference through its 30 Under 30 list. The 30 Under 30 Asia list for 2024 saw two Sri Lankans recognised and this week The Sunday Morning Brunch chatted with one of them – tech entrepreneur Lilan Dayananda.",
+        link: "https://www.themorning.lk/articles/MJd1syvb8TIK7DviAyo6",
+        backup_image: "assets/img/impact/forbes/backup/the-morning.png",
+      },
+      {
+        date: "May 24, 2024",
+        source_name: "News Asia",
+        source_link: "https://newsasia.lk/",
+        title: "Sri Lankan Entrepreneur Lilan Dayananda makes Forbes’ “30 Under 30 Asia” List",
+        image: "assets/img/impact/forbes/news-asia.png",
+        description:
+          "Lilan Dayananda, a  Sri Lankan entrepreneur, has earned a distinguished place on Forbes Magazine’s “30 Under 30 Asia” list in the social impact category. This list, now in its ninth edition, celebrates young innovators under 30 who are transforming their industries and creating forward-thinking solutions to navigate changing business landscapes. The Asia list is part of the broader Forbes 30 Under 30 franchise, which has been highlighting young achievers for over a decade.",
+        link: "https://newsasia.lk/sri-lankan-entrepreneur-lilan-dayananda-makes-forbes-30-under-30-asia-list/",
+        backup_image: "assets/img/impact/forbes/backup/news-asia.png",
+      },
+      {
+        date: "",
+        source_name: "Instagram",
+        source_link: "https://www.instagram.com/",
+        title: "Sri Lankan entrepreneur Lilan Dayananda featured in Forbes 30 Under 30 list",
+        image: "assets/img/impact/forbes/instagram.png",
+        description:
+          'Lilan Dayananda who is the co-founder and CEO of Elzian Agro has been featured in the 9th edition of Forbes\' "30 Under 30 Asia" list under the social impact category. His company is a Sri Lankan startup that uses technologies like IoT devices and AI to help small farmers optimize their harvests and deal with climate change. Their services include weather and soil monitoring systems, pest detection and tracing products on the supply chain.',
+        link: "https://www.instagram.com/p/C7Ly5MNB7CX/?utm_source=ig_web_copy_link",
+        backup_image: "assets/img/impact/forbes/backup/instagram.png",
+      },
+      {
+        date: "",
+        source_name: "Linkedin",
+        source_link: "https://www.linkedin.com/feed/?nis=true",
+        title:
+          "Honored with the distinction of being selected for the Forbes 30 Under 30: Social Impact List.",
+        image: "assets/img/impact/forbes/linkedin.png",
+        description:
+          "I am delighted to share with you all that I have been honored with the distinction of being selected for the Forbes 30 Under 30: Social Impact List.In its 9th year now, the Asia list is a continuation of the global expansion of the Forbes 30 Under 30 franchise, which started over a decade ago. It’s a franchise that has had amazing alumni such as Melanie Perkins from Canva, Palmer Luckey from Oculus VR, Evan Spiegel and Bobby Murphy from Snap Inc., Kevin Aluwi from Gojek and K-pop band BTS to name just a few.",
+        link: "https://www.linkedin.com/posts/lilanofficial_forbesunder30-forbesu30asia-sdgs-activity-7196917504961646592-N6jv/?utm_source=share&utm_medium=member_desktop",
+        backup_image: "assets/img/impact/forbes/backup/linkedin.png",
+      },
+      {
+        date: "",
+        source_name: "Forbes",
+        source_link: "https://www.forbes.com/",
+        title: "Lilan Dayananda",
+        image: "assets/img/impact/forbes/forbes.png",
+        description:
+          "Lilan Dayananda is a social tech entrepreneur and cofounder and CEO of Elzian Agro, a Sri Lankan startup that uses technologies like IoT devices and AI to help small farmers optimize their harvests and deal with climate change. Their services include weather and soil monitoring systems, pest detection and tracing products on the supply chain. Dayananda's work earned him a nomination for the Queen's Commonwealth Trust Youth Award in 2022, and Elzian Agro's Pest Prediction System was given the Merit Award at the SLASSCOM National Ingenuity Awards 2022",
+        link: "https://www.forbes.com/30-under-30/2024/asia/social-impact?profile=lilan-dayananda",
+        backup_image: "assets/img/impact/forbes/backup/forbes.png",
+      },
+      {
+        date: "",
+        source_name: "Forbes",
+        source_link: "https://www.forbes.com/",
+        title: "Lilan Dayananda",
+        image: "assets/img/impact/forbes/forbes-profile.png",
+        description:
+          "Lilan Dayananda is a social tech entrepreneur and cofounder and CEO of Elzian Agro, a Sri Lankan startup that uses technologies like IoT devices and AI to help small farmers optimize their harvests and deal with climate change. Their services include weather and soil monitoring systems, pest detection and tracing products on the supply chain. Dayananda's work earned him a nomination for the Queen's Commonwealth Trust Youth Award in 2022, and Elzian Agro's Pest Prediction System was given the Merit Award at the SLASSCOM National Ingenuity Awards 2022.",
+        link: "https://www.forbes.com/profile/lilan-dayananda/?list=30under30-asia-social-impact/&sh=47305d8b5e76",
+        backup_image: "assets/img/impact/forbes/backup/forbes-profile.png",
+      },
+    ],
+  },
+  {
+    filter: "filter-recognition",
+    title: "New Generation Asia: Social Entrepreneur of the Year",
+    image: "assets/img/impact/new-generation-asia/lanka-news-network.png",
+    description:
+      "Lilan Dayananda, the dynamic Managing Director and CEO of Elzian Agro, was honored as the Social Entrepreneur of the Year at the Fifth New Generation Asia Awards held at the Shangri-La Hotel in Colombo.",
+    slides: [
+      {
+        date: "",
+        source_name: "",
+        source_link: "",
+        title: "",
+        image: "assets/img/impact/",
+        description: "",
+        link: "",
+        backup_image: "assets/img/impact/",
       },
     ],
   },
