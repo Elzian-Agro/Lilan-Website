9f53c2c27418796c919d5ba61fdf6c98f255a471
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-09-03 13:19:40 +0530

    add impact

--- constants/impacts.js
+++ constants/impacts.js
@@ -3,8 +3,7 @@ const impacts = [
     filter: "filter-recognition",
     title: "Forbes 30 under 30: Social Impact List",
     image: "assets/img/impact/forbes/ada-derana.png",
-    description:
-      "Sri Lankan entrepreneur Lilan Dayananda has been honored with a coveted spot on Forbes Magazine’s “30 Under 30 Asia” list in the social impact sector.",
+    description: "",
     slides: [
       {
         date: "May, 29, 2024",
@@ -108,7 +107,7 @@ const impacts = [
       {
         date: "",
         source_name: "Linkedin",
-        source_link: "https://www.linkedin.com/feed/?nis=true",
+        source_link: "https://www.linkedin.com/in/lilanofficial/",
         title:
           "Honored with the distinction of being selected for the Forbes 30 Under 30: Social Impact List.",
         image: "assets/img/impact/forbes/linkedin.png",
@@ -145,8 +144,7 @@ const impacts = [
     filter: "filter-recognition",
     title: "New Generation Asia: Social Entrepreneur of the Year",
     image: "assets/img/impact/new-generation-asia/lanka-news-network.png",
-    description:
-      "Lilan Dayananda, the dynamic Managing Director and CEO of Elzian Agro, was honored as the Social Entrepreneur of the Year at the Fifth New Generation Asia Awards held at the Shangri-La Hotel in Colombo.",
+    description: "",
     slides: [
       {
         date: "August 16, 2024",
@@ -173,14 +171,160 @@ const impacts = [
       {
         date: "",
         source_name: "LinkedIn",
-        source_link: "https://www.linkedin.com/feed/?nis=true",
-        title:
-          "Honored as the Social Entrepreneur of the Year at the Fifth New Generation Asia Awards",
+        source_link: "https://www.linkedin.com/in/lilanofficial/",
+        title: "Honored as the Social Entrepreneur of the Year at the Fifth New Generation Asia Awards",
         image: "assets/img/impact/new-generation-asia/linkedin.png",
         description:
           "I am delighted to announce that I have been honored as the Social Entrepreneur of the Year at the Fifth New Generation Asia Awards, at the Shangri-La Hotel in Colombo.",
         link: "https://www.linkedin.com/posts/lilanofficial_youth-startup-economy-activity-7216735742062944256-aduN/?utm_source=share&utm_medium=member_desktop",
         backup_image: "assets/img/impact/new-generation-asia/backup/linkedin.png",
+      },
+    ],
+  },
+  {
+    filter: "filter-recognition",
+    title: "Commonwealth Youth Award Winner",
+    image: "assets/img/impact/commonwealth/linkedin.png",
+    description: "",
+    slides: [
+      {
+        date: "May 16, 2022",
+        source_name: "Your Commonwealth",
+        source_link: "https://yourcommonwealth.org/",
+        title: "Putting an end to world hunger through smart agronomy solutions",
+        image: "assets/img/impact/commonwealth/commonwealth.png",
+        description:
+          "Lilan Dayananda is a co-founder and the CEO at Elzian Agro. His organisation has been tackling world hunger through award-winning smart agronomy solutions. These innovations provide soil and weather monitoring systems to farmers who are affected by extreme weather changes and pests and who also have limited resources to service their farms. 25-year- old Commonwealth Correspondent Aminat Awogun from Nigeria spoke with Lilan, who is a 2022 Commonwealth Youth Awards Finalist.",
+        link: "https://yourcommonwealth.org/economic-development/putting-an-end-to-world-hunger-through-smart-agronomy-solutions/",
+        backup_image: "assets/img/impact/commonwealth/backup/commonwealth.png",
+      },
+      {
+        date: "",
+        source_name: "",
+        source_link: "",
+        title: "Commonwealth Youth Awards 2022",
+        image: "assets/img/impact/commonwealth/commonwealth-award.png",
+        description:
+          "The Commonwealth Youth Awards for Excellence in Development Work aim to recognise the contribution young people aged 15–29 make towards promoting and enhancing democracy and achieving the 17 Sustainable Development Goals (SDGs). It raises the profile of committed and innovative Commonwealth youth who are working hard to address pressing issues such as poverty and conflict.",
+        link: "https://production-new-commonwealth-files.s3.eu-west-2.amazonaws.com/s3fs-public/2022-03/OFFICIAL%202022%20Commonwealth%20Youth%20Awards%20Booklet.pdf",
+        backup_image:
+          "assets/img/impact/commonwealth/backup/OFFICIAL 2022 Commonwealth Youth Awards Booklet.pdf",
+      },
+      {
+        date: "March 01, 2022",
+        source_name: "The Commonwealth",
+        source_link: "https://thecommonwealth.org/",
+        title: "20 finalists announced for Commonwealth Youth Awards 2022",
+        image: "assets/img/impact/commonwealth/the-commonwealth.png",
+        description:
+          "Twenty extraordinary young people, including inventors, activists and entrepreneurs, from 17 countries have been announced as finalists for this year’s Commonwealth Youth Awards...",
+        link: "https://thecommonwealth.org/news/20-finalists-announced-commonwealth-youth-awards-2022",
+        backup_image: "assets/img/impact/commonwealth/backup/the-commonwealth.png",
+      },
+      {
+        date: "",
+        source_name: "LinkedIn",
+        source_link: "https://www.linkedin.com/in/lilanofficial/",
+        title: "Commonwealth Youth Awards",
+        image: "assets/img/impact/commonwealth/linkedin.png",
+        description:
+          "I recently had the opportunity to capture a moment with my esteemed Commonwealth Youth Award, a prestigious recognition bestowed upon young individuals aged 15 to 29 who have demonstrated exceptional dedication to enhancing democracy and furthering the cause of the 17 Sustainable Development Goals (SDGs). These awards represent a vital facet of The Commonwealth Secretariat's overarching mission to empower, engage, and inspire young people, all while amplifying their voices and achievements throughout the 56 member states.",
+        link: "https://www.linkedin.com/posts/lilanofficial_commonwealth-youth-commonwealthyouthawards-activity-7117812520861519873-n2Yb/?utm_source=share&utm_medium=member_desktop",
+        backup_image: "assets/img/impact/commonwealth/backup/linkedin.png",
+      },
+      {
+        date: "",
+        source_name: "International Youth Change Maker - IYCM",
+        source_link: "https://web.facebook.com/IYcmBd",
+        title: "Commonwealth Youth Awards Winner 2022",
+        image: "assets/img/impact/commonwealth/commonwealth-award-sl.png",
+        description:
+          "As the Co-Founder and CEO of ‘Elzian Agro’ Lilan offers rural farmers smart agronomy solutions to increase crop yield organically and helps them respond to significant changes in soil and weather and potential pest threats through data-based prediction reports. The project has reached more than 50,000 farmers and supports government bodies in Sri Lanka to better understand the importance of modern smart agronomy solutions.",
+        link: "https://web.facebook.com/100064883162038/posts/4751834144927957/",
+        backup_image: "assets/img/impact/commonwealth/backup/commonwealth-award-sl.png",
+      },
+      {
+        date: "February 10, 2022",
+        source_name: "Colombo Gazette",
+        source_link: "https://colombogazette.com/",
+        title: "Sri Lankan shortlisted for Commonwealth Youth Awards 2022",
+        image: "assets/img/impact/commonwealth/colombo-gazette.png",
+        description:
+          "Twenty extraordinary young people, including inventors, activists and entrepreneurs, from 17 countries have been announced as finalists for this year’s Commonwealth Youth Awards.",
+        link: "https://colombogazette.com/2022/02/10/sri-lankan-shortlisted-for-commonwealth-youth-awards-2022/",
+        backup_image: "assets/img/impact/commonwealth/backup/colombo-gazette.png",
+      },
+    ],
+  },
+  {
+    filter: "filter-recognition",
+    title: "Top 10 Youth Entrepreneurs of Global-South",
+    image: "assets/img/impact/youth-entrepreneurs/south-south-galaxy.png",
+    description: "",
+    slides: [
+      {
+        date: "July 5, 2022",
+        source_name: "South-South Galaxy",
+        source_link: "https://southsouth-galaxy.org/",
+        title:
+          "Top Youth Entrepreneurs Selected for Final Youth4South Entrepreneurship Competition, to be held in Bangkok in September",
+        image: "assets/img/impact/youth-entrepreneurs/south-south-galaxy.png",
+        description:
+          "More than twenty young entrepreneurs from 15 developing countries came together for the first-round pitching of the Youth4South Entrepreneurship Competition on 23 and 24 June 2022. Based on the competitive live pitching over the two days, 10 initiatives emerged as the most environmentally friendly and financially sustainable ones, which made to the main competition at the Global South-South Development Expo 2022 to be held in Bangkok, Thailand, in September.",
+        link: "https://southsouth-galaxy.org/news/top-youth-entrepreneurs-selected-for-final-youth4south-entrepreneurship-competition/",
+        backup_image: "assets/img/impact/youth-entrepreneurs/backup/south-south-galaxy.png",
+      },
+    ],
+  },
+  {
+    filter: "filter-recognition",
+    title: "Top Meaningful Business Founders",
+    image: "assets/img/impact/meaningful-business/linkedin.png",
+    description: "",
+    slides: [
+      {
+        date: "",
+        source_name: "Meaningful Business",
+        source_link: "https://www.linkedin.com/company/meaningful-business/posts/",
+        title: "Meaningful Business 100 for 2023",
+        image: "assets/img/impact/meaningful-business/meaningful-business.png",
+        description:
+          "These leaders are setting the bar for combining profit and purpose, helping to tackle the world’s most pressing social and environmental issues. Your annual source of inspiration is here.",
+        link: "https://www.linkedin.com/posts/meaningful-business_meaningfulbusiness-2023mb100-mb100-activity-7117842154596495360-takh/?utm_source=share&utm_medium=member_desktop",
+        backup_image: "assets/img/impact/meaningful-business/backup/meaningful-business.png",
+      },
+      {
+        date: "",
+        source_name: "LinkedIn",
+        source_link: "https://www.linkedin.com/in/lilanofficial/",
+        title: "Recognised by Meaningful Business on the 2023",
+        image: "assets/img/impact/meaningful-business/linkedin.png",
+        description:
+          "The 2023 MB100 are an outstanding group of leaders combining profit and purpose to help achieve the UN Global Goals across five key areas: Durability, Impact, Innovation, Leadership and Scope.",
+        link: "https://www.linkedin.com/posts/lilanofficial_mb100-meetthemb100-2023mb100-activity-7117863318945492992-8DK_/?utm_source=share&utm_medium=member_desktop",
+        backup_image: "assets/img/impact/meaningful-business/backup/linkedin.png",
+      },
+      {
+        date: "",
+        source_name: "Meaningful Business",
+        source_link: "https://meaningful.business/",
+        title: "Lilan Dayananda Profile",
+        image: "assets/img/impact/meaningful-business/meaningful-business-profile.png",
+        description:
+          "Lilan Dayananda is an international award-winning young social tech entrepreneur. For years, his social initiative Elzian Agro has offered customised monitoring information and automation solutions through trending digital technologies to optimise customers’ harvest and productively utilise their resources, improving food security and climate action. ",
+        link: "https://meaningful.business/team/lilan-dayananda/",
+        backup_image: "assets/img/impact/meaningful-business/backup/meaningful-business-profile.png",
+      },
+      {
+        date: "",
+        source_name: "",
+        source_link: "",
+        title: "The 2023 Meaningful Business 100 (MB100)",
+        image: "assets/img/impact/meaningful-business/meaningful-business-mb100.png",
+        description:
+          "The 2023 MB100 are an outstanding group of leaders combining profit and purpose. Curated by an expert panel of 29 judges, the winners were chosen from over 800 applications, representing 93 countries, and were scored across five key areas: Durability, Impact, Innovation, Leadership and Scope.",
+        link: "https://meaningful.business/mb100-2023/",
+        backup_image: "assets/img/impact/meaningful-business/backup/meaningful-business-mb100.png",
       },
     ],
   },
