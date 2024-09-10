8c7cf0ca4e14b1d62b55afd93fb28799961c9aa9
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-09-10 09:50:02 +0530

    add blogs and podcast pages

---
+++ constants/podcasts.js
@@ -0,0 +1,7 @@
+const podcasts = [
+  {
+    title: "Forbes 30 under 30: Social Impact List",
+    image: "assets/img/impact/recognition/forbes/ada-derana.png",
+    url: "https://agro.elzian.com/podcast/podcast-main.html",
+  },
+];
